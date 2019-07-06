3d23b3acbbb957abd3ae333ee51322b2ec7b758b
Lucas de Moura Rodrigues <lucas.rodrigues@xpi.com.br>
2019-07-05 21:22:50 -0300

    adiciona teste unitarios para cadidato

--- candidates/tests.py
+++ candidates/tests.py
@@ -1,1 +1,15 @@
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class CandidateTest(APITestCase):
+    def test_candidate_not_found(self):
+        data = {'name': 'jorge'}
+        response = self.client.get(reverse('candidates'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_candidate_found(self):
+        data = {'name': 'lucas'}
+        response = self.client.get(reverse('candidates'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
